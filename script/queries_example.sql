9c1918e712edb7785447e32f3c835f171b80766c
saisathvik99 <b22ec035@kitsw.ac.in>
2025-08-17 01:15:31 +0530

    Update queries_example.sql

--- script/queries_example.sql
+++ script/queries_example.sql
@@ -1,1 +1,11 @@
-
+-- Example Queries
+SELECT title, available_books FROM Books WHERE available_books > 0;
+SELECT full_name, join_date FROM Members
+--Fine calculation
+SELECT 
+M.full_name, B.title, 
+DATEDIFF(day, return_date, actual_return_date) * 10 AS fine
+FROM Issued_Books I
+JOIN Members M ON I.member_id = M.mem_id
+JOIN Books B ON I.book_id = B.book_id
+WHERE actual_return_date > return_date;
